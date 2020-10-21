272117636f337094ee060b70d1f5203da8e368e7
datajoergen <maller2650@gmail.com>
2020-10-21 15:46:15 +0200

    Initial commit

---
+++ src/main/java/com/madskchristensen/testjenkinsec2/TestJenkinsEc2Application.java
@@ -0,0 +1,13 @@
+package com.madskchristensen.testjenkinsec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsEc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinsEc2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/madskchristensen/testjenkinsec2/TestJenkinsEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madskchristensen.testjenkinsec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsEc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
