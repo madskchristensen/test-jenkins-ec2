4e733ec6e02fef68206bceac340d6bf1a7b17a06
Mads <maller2650@gmail.com>
2020-10-29 15:17:27 +0100

    -- test med ny ec2 server med elastic ip

--- src/main/java/com/madskchristensen/testjenkinsec2/HomeController.java
+++ src/main/java/com/madskchristensen/testjenkinsec2/HomeController.java
@@ -1,19 +1,22 @@
 package com.madskchristensen.testjenkinsec2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-
+    public String index(Model model, HttpServletRequest request) {
+        model.addAttribute("request", request);
         return "index";
     }
 
     @GetMapping("/jenkins")
     public String jenkinsLandingPage() {
-        return "redirect:http://ec2-3-234-243-241.compute-1.amazonaws.com:8080/";
+        return "redirect:http://54.81.145.200:8080/";
     }
 }
