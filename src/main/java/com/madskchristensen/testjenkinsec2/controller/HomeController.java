efa0381b5b87d1eb2d6349ea7163445a818e108e
datajoergen <maller2650@gmail.com>
2020-11-11 15:15:53 +0100

    -- test af multiline shell command i jenkins

--- src/main/java/com/madskchristensen/testjenkinsec2/controller/HomeController.java
+++ src/main/java/com/madskchristensen/testjenkinsec2/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+
+/*
+    Dette er en controller
+ */
+
 @Controller
 public class HomeController {
 
