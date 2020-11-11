a7e937de64993bac181650d9022e9170f6e76bfc
datajoergen <maller2650@gmail.com>
2020-11-11 13:43:13 +0100

    -- tester jenkins efter 12 dage

--- src/main/java/com/madskchristensen/testjenkinsec2/HomeController.java
+++ src/main/java/com/madskchristensen/testjenkinsec2/HomeController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
-
+/*
+    En controller
+ */
 @Controller
 public class HomeController {
 
